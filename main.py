1495c42c63a3611cad0f8eb155911d4f5d0e3070
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2016-08-17 23:27:26 +0200

    Comment code

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ import pickle
 import os
 from getpass import getpass
 
+# redirect log to stdout and make logger available for script
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger('AutoIV')
+
+# initialize PGOAPI
 api = PGoApi()
 
+# collect login info via cmd-line arguments and user input
 def collect_login_info():
     parser = argparse.ArgumentParser(
         description='Rename pokemon according to their IVs')
@@ -34,6 +38,7 @@ def collect_login_info():
 
     return args.auth, args.username, password
 
+# send request via PGOAPI to retrieve inventory
 def retrieve_inventory(api, auth, username, password):
     if api.login(provider=auth,
                  username=username,
@@ -47,16 +52,21 @@ def retrieve_inventory(api, auth, username, password):
         log.debug('Failed to retrieve inventory.')
         return {}
 
+# we are only interested in the inventory_items of the response object
 def extract_items(response):
     inventory = response['responses']['GET_INVENTORY']['inventory_delta']
     return inventory['inventory_items']
 
+# of the inventory items we only care for actual pokemon
 def filter_for_pokemon(items):
     for item in items:
         maybe_pokemon = item['inventory_item_data'].get('pokemon_data', {})
+        # since eggs are also stored under the key 'pokemon_data' check for the
+        # presence of pokemon_id
         if 'pokemon_id' in maybe_pokemon:
             yield maybe_pokemon
 
+# avoid unnecessary nick-naming of pokemon that were rated in the past
 def remove_rated(pokemon, rated_pokemon):
     for pkmn in pokemon:
         if pkmn['id'] in rated_pokemon:
@@ -64,12 +74,14 @@ def remove_rated(pokemon, rated_pokemon):
         else:
             yield pkmn
 
+# load list of pokemon from disk that have been rated in the past 
 def load_rated_pkmn():
     if os.path.exists('.rated_pkmn_list.pkl'):
         return pickle.load(open('.rated_pkmn_list.pkl', 'rb'))
     else:
         return []
 
+# store list of pokemon that have been rated to disk
 def store_rated_pkmn(list):
     pickle.dump(list, open('.rated_pkmn_list.pkl', 'wb'))
 
@@ -83,18 +95,24 @@ log.info("Looping through {0} pokemon..".format(amount))
 i = 0
 try:
     for pkmn in pokemon:
+        # build nickname in the format of ATK-DEF-STAMINA
         attack = pkmn.get('individual_attack', 0)
         defense = pkmn.get('individual_defense', 0)
         stamina = pkmn.get('individual_stamina', 0)
         nick = "{0}-{1}-{2}".format(attack, defense, stamina)
         id = pkmn['id']
         i = i + 1
+        # nickname pokemon
         api.nickname_pokemon(pokemon_id=id, nickname=nick)
+
         log.info("(%(i)s/%(amount)s) Renamed %(id)s to %(nick)s" % locals())
+        # add pkmn to list of rated pkmn
         rated_pkmn.append(id)
+        # wait a little so we do not spam the API
         sleep_time = randint(3, 10)
         log.info("Waiting for {0} seconds".format(sleep_time))
         sleep(sleep_time)
 finally:
+    # make sure we store the new list of rated pokemon
     store_rated_pkmn(rated_pkmn)
 log.info("Finished renaming Pokemon according to IVs.")
