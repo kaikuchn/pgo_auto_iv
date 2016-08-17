bd33db09f06cefe97852d6535a37fc5690e178a7
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2016-08-17 23:15:50 +0200

    Track already rated pkmn by their id
    Store the list of rated pokemons in .rated_pkmn_list.pkl to remember rated
    pokemon throughout multiple sessions

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from random import randint
 import logging
 import sys
 import argparse
+import pickle
+import os
 from getpass import getpass
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -55,25 +57,44 @@ def filter_for_pokemon(items):
         if 'pokemon_id' in maybe_pokemon:
             yield maybe_pokemon
 
+def remove_rated(pokemon, rated_pokemon):
+    for pkmn in pokemon:
+        if pkmn['id'] in rated_pokemon:
+            log.debug('Skipping {0}'.format(pkmn))
+        else:
+            yield pkmn
+
+def load_rated_pkmn():
+    if os.path.exists('.rated_pkmn_list.pkl'):
+        return pickle.load(open('.rated_pkmn_list.pkl', 'rb'))
+    else:
+        return []
+
+def store_rated_pkmn(list):
+    pickle.dump(list, open('.rated_pkmn_list.pkl', 'wb'))
 
 (auth, user, password) = collect_login_info()
 response = retrieve_inventory(api, auth, user, password)
 items = extract_items(response)
-pokemons = [pkmn for pkmn in filter_for_pokemon(items)]
-amount = len(pokemons)
+rated_pkmn = load_rated_pkmn()
+pokemon = [pkmn for pkmn in remove_rated(filter_for_pokemon(items), rated_pkmn)]
+amount = len(pokemon)
 log.info("Looping through {0} pokemon..".format(amount))
 i = 0
-for pkmn in pokemons:
-    attack = pkmn.get('individual_attack', 0)
-    defense = pkmn.get('individual_defense', 0)
-    stamina = pkmn.get('individual_stamina', 0)
-    nick = "{0}-{1}-{2}".format(attack, defense, stamina)
-    id = pkmn['id']
-    i = i + 1
-    if pkmn.get('nickname', '') != nick:
+try:
+    for pkmn in pokemon:
+        attack = pkmn.get('individual_attack', 0)
+        defense = pkmn.get('individual_defense', 0)
+        stamina = pkmn.get('individual_stamina', 0)
+        nick = "{0}-{1}-{2}".format(attack, defense, stamina)
+        id = pkmn['id']
+        i = i + 1
         api.nickname_pokemon(pokemon_id=id, nickname=nick)
         log.info("(%(i)s/%(amount)s) Renamed %(id)s to %(nick)s" % locals())
+        rated_pkmn.append(id)
         sleep_time = randint(3, 10)
         log.info("Waiting for {0} seconds".format(sleep_time))
         sleep(sleep_time)
+finally:
+    store_rated_pkmn(rated_pkmn)
 log.info("Finished renaming Pokemon according to IVs.")
